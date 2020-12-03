26e6c33feabae58b596136f7e9d437777609e35b
Philipp Dawid <phildwd@gmail.com>
2020-12-03 20:53:00 +0100

    cookies set problems with database

--- db.js
+++ db.js
@@ -1,6 +1,20 @@
-var spicedPg = require('spiced-pg');
-var db = spicedPg('postgres:phil:postgres@localhost:5432/oscars');
+const spicedPg = require('spiced-pg');
+const db = spicedPg('postgres:postgres:postgres@localhost:5432/petition');
 
-module.exports.getActors = () => {
-    return db.query("SELECT * FROM oscars");
+module.exports.getUserdata = () => {
+    return db.query(`SELECT * FROM petition`);
 };
+
+module.exports.updateUserData = (first, last, signature) => {
+    const q = `INSERT INTO petition(first, last, signature) VALUES ($1, $2, $3)`
+    const s = `SELECT first, last FROM petition ORDER BY id;`
+    const params = [first, last, signature];
+    return db.query(q, params)    
+};
+exports.ifSigned = id => {
+    const q = `SELECT id FROM signatures WHERE id = $1`;
+    const params = [id];
+    return db.query(q, params);
+};
+
+
--- index.js
+++ index.js
@@ -1,46 +1,88 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const cookieParser = require('cookie-parser');
 
 const hb = require("express-handlebars");
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
+const {
+    getUserdata,
+    updateUserData,
+    ifSigned
+} = require("./db");
+
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(cookieParser());
 app.use(express.static("./public"));
 
 
 
-app.use((req, res, next)=> {
-    console.log("--------------");
-    console.log(`${req.method} request coming on route ${req.url}`);
-    console.log("--------------");
-    next();
+// app.use((req, res, next)=> {
+//     console.log("--------------");
+//     console.log(`${req.method} request coming on route ${req.url}`);
+//     console.log("--------------");
+//     next();
 
-})
+// })
 
-
-app.get("/", (req, res)=>{
-    console.log("user requesting GET / route");
-    res.render("petition", {
-        layout: "main",//when you have called you layout file main, 
-                        //you may omit spcifying this here, hb express  
-                        //will by default look for a layout file with that name
-    });
-});
 
 app.get("/petition", (req, res) => {
     console.log("user requesting GET / petition");
+    if("authenticated" == true){
+        res.render("thanks",{
+            layout: "main",
+        });
+    }
     res.render("petition",{
         layout: "main",
     });
+    
 });
 
 app.post("/petition", (req, res) => {
-    console.log("POST petition was made")
+    console.log("POST petition was made");
+    console.log("req.params",req.params);
+    console.log("req.body", req.body);
+    const { first, last, signature } = req.body;
+   
+    // let signature = req.body.signature;
+    // let first = req.body.first;
+    // let last =  req.body.last;
+    
+    db.updateUserData(first, last, signature)
+        .then(()=>{
+            if("authenticated" == false){
+                console.log('req.cookies: ', req.cookies);
+                res.redirect("/petition");
+
+            }
+            res.cookie("authenticated", true)
+            console.log('req.cookies: ', req.cookies);
+            res.redirect("/thanks");
+        })
+        .catch((err)=> {
+            console.log("error in updateUserData",err)
+            res.cookie("authenticated", false);
+            console.log('req.cookies: ', req.cookies);
+            res.redirect("/petition");
+        })
 });
 
 app.get("/thanks", (req, res) => {
     console.log("user requesting GET / thanks");
+    if("authenticated" == false){
+        res.render("petition",{
+            layout: "main",
+    });
+    }
     res.render("thanks",{
         layout: "main",
     });
