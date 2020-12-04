3273f1403bb8bd43b5f5429f109131234bcae42d
Philipp Dawid <phildwd@gmail.com>
2020-12-04 17:40:44 +0100

    add canvas

---
+++ public/script.js
@@ -0,0 +1,50 @@
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const drawSig = document.getElementById("canvas");
+const ctx = drawSig.getContext("2d");
+
+const rect = drawSig.getBoundingClientRect();
+
+drawSig.addEventListener("mousedown", e => {
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    isDrawing = true;
+});
+
+drawSig.addEventListener("mousemove", e => {
+    if (isDrawing === true) {
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+    }
+});
+
+window.addEventListener("mouseup", e => {
+    if (isDrawing === true) {
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+const button = $("button");
+
+const signature = document.getElementById("signature");
+
+button.on("click", function() {
+    let canvasValue = drawSig.toDataURL();
+    signature.value = canvasValue;
+});
